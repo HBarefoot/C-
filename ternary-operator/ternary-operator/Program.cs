4fcb1feb4c00229be1f9d26036509d853a63eb91
Henry Barefoot <henrybarefoot@henrys-mbp.attlocal.net>
2017-10-13 15:31:00 -0400

    c# course form microsoft virtual academy-fundamentals

---
+++ ternary-operator/ternary-operator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ternary_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway.");
+            Console.Write("Choose a door: 1, 2, 3: ");
+            string userValue = Console.ReadLine();
+
+            // evaluation usiong ternary operator
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+
+
+            //Console.WriteLine("You won a {0}.", message);
+            Console.WriteLine("Youe entered: {0}, therefore you won a {1}.", userValue, message);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ variables/variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace variables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name ?");
+            Console.WriteLine("type ypur first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.WriteLine("type your last name: ");
+            myLastName = Console.ReadLine();
+
+
+            Console.WriteLine("Hello " + myFirstName + " " + myLastName);
+        }
+    }
+}
