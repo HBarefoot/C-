201c917896b24d2a62fac1fcb6e9101be592163d
Henry Barefoot <henrybarefoot@henrys-mbp.attlocal.net>
2017-10-15 18:47:41 -0400

    c# course from MVA (practices in VS code)

---
+++ Program/Program/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+
+
+            Console.WriteLine("Value: " + numbers[9] + ".");
+            Console.WriteLine("Length of my array: " + numbers.Length + ".");
+            Console.ReadLine();
+
+
+            //array declarations
+            //int[] numbers = new int[] { 4, 8, 12, 454, 66, 7, 77 };
+
+            string[] names = new string[] { "henry", "pepe", "marcos", "fernan", "lester" };
+
+            // for loop
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("For loop method: " + names[i] + ".");
+            }
+
+            // foreach loop
+            foreach (string name  in names)
+            {
+                Console.WriteLine("For each method " + name + ".");
+            }
+
+
+            string zig = "You can get what you want out of life" + 
+                " if you help enough other people get what they want.";
+
+            char[] charArray = zig.ToCharArray();
+
+            Array.Reverse(charArray);
+
+
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling HelloWorld() method
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+    }
+}
