0a1b9dbb55b2a5db1c9aba0fbc4c471babb1fc78
Henry Barefoot <henrybarefoot@henrys-mbp.attlocal.net>
2017-10-16 14:34:51 -0400

    c# course MVA

---
+++ DatesAndTimes/DatesAndTimes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Date manipulation
+
+            //DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
+
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //Console.WriteLine(myValue.AddHours(-3).ToLongTimeString());
+            //Console.WriteLine(myValue.Month);
+
+            //DateTime myBirthay = new DateTime(1987, 09, 30);
+            //Console.WriteLine(myBirthay.ToShortDateString());
+
+            //DateTime myBirthday = DateTime.Parse("09/30/1987");
+            //TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            //Console.WriteLine(myAge.TotalDays);
+
+
+            Console.ReadLine();
+        }
+    }
+}
